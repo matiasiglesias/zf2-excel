ea363796db794c63a5e8f41002a5863de39eb285
Adao Goncalves <adao@adao.eti.br>
2015-02-02 11:18:50 -0200

    commit inicial

---
+++ Module.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * ZF2Excel Module (https://github.com/adaoex/zf2-excel)
+ * 
+ * @author      Adão Gonçalves <adao@adao.eti.br>
+ * @link        https://github.com/adaoex/zf2-base for the canonical source repository
+ * @copyright   Copyright(c) 2015 (http://adao.eti.br)
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace ZF2Excel;
+
+class Module {
+
+    public function getConfig() {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig() {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+}
+
---
+++ config/module.config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * ZF2Excel Module (https://github.com/adaoex/zf2-excel)
+ * 
+ * @author      Adão Gonçalves <adao@adao.eti.br>
+ * @link        https://github.com/adaoex/zf2-base for the canonical source repository
+ * @copyright   Copyright(c) 2015 (http://adao.eti.br)
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace ZF2Excel;
+
+return array(
+    
+);
